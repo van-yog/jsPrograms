e1bf80ef510be04285716137dd35735efdc53f92
Ivan Kharchenko <ikharchenko2008@gmail.com>
2020-02-06 22:32:32 +0200

    add mobile version

--- programs/program3.js
+++ programs/program3.js
@@ -1,9 +1,38 @@
 let windowGame = document.querySelector(".window__game");
 let shape = document.querySelector("#shape");
-let body = document.querySelector("body");
-let rect = windowGame.getBoundingClientRect();
-console.log(`ширина ${rect.width} высота ${rect.height}`);
 
+windowGame.addEventListener("click", e => {
+  if (e.target.tagName == "SPAN") {
+    console.log(e.target.tagName);
+  }
+  let x = e.offsetX == undefined ? e.layerX : e.offsetX;
+  let y = e.offsetY == undefined ? e.layerY : e.offsetY;
+  let gameField = windowGame.getBoundingClientRect();
+  console.log(`ширина ${gameField.width} высота ${gameField.height}`);
+  console.log(x + "x" + y);
+
+  //  Проверка выходет ли круг за границы игрового поля
+  //  Очередность проверки  - левый край, верх, правый край, низ
+  if (x < 30) {
+    x = 30;
+  }
+  if (y < 30) {
+    y = 30;
+  }
+  if (x + 30 > gameField.width) {
+    x = gameField.width - 30;
+  }
+  if (y + 30 > gameField.height) {
+    y = gameField.height - 30;
+  }
+
+  shape.style.top = y - 25 + "px";
+  shape.style.left = x - 25 + "px";
+
+  console.log(x + "x" + y);
+});
+
+// Функции удаления класса фигуры и добавление согласно кнопке
 function circle() {
   let shape = document.querySelector("#shape");
   shape.classList.remove("square");
@@ -24,30 +53,3 @@ function moon() {
   shape.classList.remove("square");
   shape.classList.add("moon");
 }
-
-windowGame.addEventListener("click", e => {
-  console.log(e.target);
-  let x = e.offsetX == undefined ? e.layerX : e.offsetX;
-  let y = e.offsetY == undefined ? e.layerY : e.offsetY;
-  let gameField = windowGame.getBoundingClientRect();
-  console.log(`ширина ${gameField.width} высота ${gameField.height}`);
-  console.log(x + "x" + y);
-
-  if (x < 30) {
-    x = 30;
-  }
-  if (y < 30) {
-    y = 30;
-  }
-  if (x + 30 > gameField.width) {
-    x = gameField.width - 30;
-  }
-  if (y + 30 > gameField.height) {
-    y = gameField.height - 30;
-  }
-
-  shape.style.top = y - 25 + "px";
-  shape.style.left = x - 25 + "px";
-
-  console.log(x + "x" + y);
-});
